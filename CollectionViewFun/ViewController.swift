5641d3c0fb69f06581056592484bd0a5eb50e7af
Tameika Lawrence <tameikalawrence@Tameikas-MacBook-Air.local>
2016-10-10 14:22:25 -0400

    Collection Views

--- CollectionViewFun/ViewController.swift
+++ CollectionViewFun/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     // MARK: - Collection View DataSource
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return 1000
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
